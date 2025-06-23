033929c673a93211f469ae5ec6fda925e1e9a3a8
michi-okahata <michi.okahata@outlook.com>
2025-06-23 13:43:33 -0700

    added exception/errors to primitive tools

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 import os
 
+from pydantic_core.core_schema import ErrorType
 import requests
 from dotenv import load_dotenv
 
 from fastmcp import FastMCP
+from fastmcp.exceptions import FastMCPError
 
 load_dotenv()
 mcp = FastMCP("AgentOps")
 
 HOST = os.getenv("HOST", "http://127.0.0.1:8000")  # change default for public API
-
 
 
 class State:
@@ -31,56 +32,89 @@ def auth(api_key: str):
     """Authorize using a project API key and store the resulting JWT token.
 
     Args:
-        api_key: Project API key.
+        api_key: AgentOps project API key.
+    Returns:
+        dict: Error message or success.
     """
     data = {"api_key": api_key}
-    response = requests.post(f"{HOST}/public/v1/auth/access_token", json=data).json()
-    state.set_token(response.get("bearer"))
 
-    return {"Status": "Success"}
+    try:
+        response = requests.post(f"{HOST}/public/v1/auth/access_token", json=data)
+        response.raise_for_status()
+        state.set_token(response.json().get("bearer"))
+    except Exception as e:
+        return {"error": str(e)}
+
+    return {"status": "Success."}
+
+
+def check_auth(state: State):
+    if "Authorization" not in state.headers:
+        raise KeyError("Authorization error.")
 
 
 @mcp.tool
 def get_project():
-    if not state.headers.get("Authorization"):
-        return {"Error": "Authorization error."}
-    else:
-        response = requests.get(
-            f"{HOST}/public/v1/project", headers=state.headers
-        ).json()
-        return response
+    """Get project information."""
+    check_auth(state)
+    try:
+        response = requests.get(f"{HOST}/public/v1/project", headers=state.headers)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @mcp.tool
 def get_trace(trace_id: str):
-    response = requests.get(
-        f"{HOST}/public/v1/traces/{trace_id}", headers=state.headers
-    ).json()
-    return response
+    check_auth(state)
+    try:
+        response = requests.get(
+            f"{HOST}/public/v1/traces/{trace_id}", headers=state.headers
+        )
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @mcp.tool
 def get_trace_metrics(trace_id: str):
-    response = requests.get(
-        f"{HOST}/public/v1/traces/{trace_id}/metrics", headers=state.headers
-    ).json()
-    return response
+    check_auth(state)
+    try:
+        response = requests.get(
+            f"{HOST}/public/v1/traces/{trace_id}/metrics", headers=state.headers
+        )
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @mcp.tool
 def get_span(span_id: str):
-    response = requests.get(
-        f"{HOST}/public/v1/spans/{span_id}", headers=state.headers
-    ).json()
-    return response
+    check_auth(state)
+    try:
+        response = requests.get(
+            f"{HOST}/public/v1/spans/{span_id}", headers=state.headers
+        )
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @mcp.tool
 def get_span_metrics(span_id: str):
-    response = requests.get(
-        f"{HOST}/public/v1/spans/{span_id}/metrics", headers=state.headers
-    ).json()
-    return response
+    check_auth(state)
+    try:
+        response = requests.get(
+            f"{HOST}/public/v1/spans/{span_id}/metrics", headers=state.headers
+        )
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
 
 
 if __name__ == "__main__":
